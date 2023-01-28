1828cb77ebbeba701783650456e4b91c67eb354e
dan mackinlay <dan@livingthing.org>
2023-01-28 17:45:10 +1100

    looks nicer

--- nextgen_01_lib.py
+++ nextgen_01_lib.py
@@ -1,3 +1,9 @@
 from manim import *
 from nextgen_01_defs import *
+from manim_fonts import *
 
+
+def patient_state_icon(symb, color=BLACK):
+    # with RegisterFont("Open Sans") as fonts:
+    #     return Text(symb, font=fonts[0], weight=BOLD, color=color)
+    return Text(symb, font="sans-serif", weight=BOLD, color=color)
